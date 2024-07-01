1102f230d69cc114a8478f014f0d4d9af3cff2de
haeunNoh06 <okhagrace06@naver.com>
2024-07-01 14:05:05 +0900

    #5 22-7. 소멸자

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -19,6 +19,10 @@ namespace CSPractice1
         {
             Console.WriteLine("(인스턴스)생성자 호출");
         }
+        ~Sample()
+        {
+            Console.WriteLine("소멸자 호출");
+        }
     }
     class MyMath
     {
@@ -220,6 +224,8 @@ namespace CSPractice1
             Console.WriteLine("두 번째 위치");
             Sample sample = new Sample();
             Console.WriteLine("세 번째 위치");
+
+            // #5 22-7. 소멸자
 
         }
     }
